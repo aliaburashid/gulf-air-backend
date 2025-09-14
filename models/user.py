a7306aae37a74fe0b5d2743a81c115dfe4563399
aliaburashid <fy22ayyy@leeds.ac.uk>
2025-09-14 14:40:06 +0300

    Defines the User table structure and authentication methods

--- models/user.py
+++ models/user.py
@@ -1,3 +1,4 @@
+# Defines the User table structure and authentication methods
 
 from sqlalchemy import Column, Integer, String
 from .base import BaseModel
@@ -10,10 +11,13 @@ from sqlalchemy.orm import relationship
 # Creating a password hashing context using bcrypt
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# Inherits from BaseModel
 class UserModel(BaseModel):
-
+    
+    # defines a user table in the database
     __tablename__ = "users"
-
+    
+    # user fields
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True)  # Each username must be unique
     email = Column(String, unique=True)  # Each email must be unique
@@ -23,15 +27,19 @@ class UserModel(BaseModel):
     phone_number = Column(String)
 
     # Relationships - a user can have multiple bookings
-    # bookings = relationship('BookingModel', back_populates='user')
-
+    bookings = relationship('BookingModel', back_populates='user')
+    
+    # password hashing: Takes a plain password and hashes it using bcrypt (passlib).
     def set_password(self, password: str):
         self.password_hash = pwd_context.hash(password)
 
     # Method to verify the password
+    # Compares a plain password with the stored hash.
+    # Returns True if it matches, False otherwise.
     def verify_password(self, password: str) -> bool:
         return pwd_context.verify(password, self.password_hash)
-
+    
+    # generates a JWT token
     def generate_token(self):        
         # Define the payload
         payload = {
@@ -39,6 +47,6 @@ class UserModel(BaseModel):
             "iat": datetime.now(timezone.utc), # Issued at time
             "sub": str(self.id), # Subject - the user ID
         }
-        # Create the JWT token
+        # Create the JWT token and encodes it using the secret key in environmnet.py
         token = jwt.encode(payload, secret, algorithm="HS256")
         return token
