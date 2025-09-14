1f0a2a6f4ef4ead0f0298e263164bae668f5795a
aliaburashid <fy22ayyy@leeds.ac.uk>
2025-09-14 15:05:50 +0300

    Add flight serializers (FlightSchema, FlightCreate, FlightUpdate)

--- serializers/flight.py
+++ serializers/flight.py
@@ -1,7 +1,11 @@
+# Defines flight information 
+
+# pydantic: a library used in FastAPI to validate and handle data easily.
 from pydantic import BaseModel, Field
 from typing import Optional, List
 from datetime import datetime
 
+# For reading/displaying flight data
 class FlightSchema(BaseModel):
     id: Optional[int] = Field(default=None)
     flight_number: str
@@ -16,8 +20,10 @@ class FlightSchema(BaseModel):
     status: str = "scheduled"
 
     class Config:
+        # allows this Pydantic model to work directly with ORM/database objects
         orm_mode = True
 
+# used when creating a new flight 
 class FlightCreate(BaseModel):
     flight_number: str
     departure_airport: str
@@ -28,8 +34,11 @@ class FlightCreate(BaseModel):
     price: float
     available_seats: int
     total_seats: int
+    # Ensures the data sent by the user is valid before saving to the database.
     status: str = "scheduled"
 
+# Used when updating an existing flight
+# All fields are optional because the user might want to update only some fields, not all of them
 class FlightUpdate(BaseModel):
     flight_number: Optional[str] = None
     departure_airport: Optional[str] = None
